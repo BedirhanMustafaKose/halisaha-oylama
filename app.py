e4d0699d72b614c8b594207796038b16020b757e
Bedirhan Mustafa Köse <111669086+BedirhanMustafaKose@users.noreply.github.com>
2025-05-01 19:12:06 +0300

    app.py

--- app.py
+++ app.py
@@ -62,7 +62,11 @@ st.subheader("📊 Anlık Ortalama Sonuçlar")
 
 if os.path.exists(data_file):
     df_all = pd.read_csv(data_file)
-    avg_scores = df_all.mean().sort_values(ascending=False)
+  for p in players:
+    if p not in df_all.columns:
+        df_all[p] = pd.NA
+
+avg_scores = df_all[players].mean().sort_values(ascending=False)
     st.table(avg_scores.round(2).reset_index().rename(columns={"index": "Oyuncu", 0: "Ortalama Puan"}))
 else:
     st.info("Henüz hiç oy verilmedi.")
